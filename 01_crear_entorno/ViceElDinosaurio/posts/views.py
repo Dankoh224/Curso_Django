3318dd91dd3d46c31bf26fd3f69a6b4e85b1d681
Danko Valderrama <danko.h224@gmail.com>
2022-06-04 11:00:03 -0400

    practicando el proceso de request & response

--- 01_crear_entorno/ViceElDinosaurio/posts/views.py
+++ 01_crear_entorno/ViceElDinosaurio/posts/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
-
+from django.http.response import HttpResponse #No olvidar!!!
 # Create your views here.
+def post_list(request):
+    return HttpResponse("Hola mundo, yo soy Vicente el ¡¡¡Dinosaurio!!!")
